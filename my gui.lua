323e83c79df1d3045b4730f5712ca2261ca313e5
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-08-10 17:53:47 +0300

    Update my gui.lua

--- my gui.lua
+++ my gui.lua
@@ -12,12 +12,6 @@ Section:NewButton("Autofarm skill tree", "ButtonInfo", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/YBA-autofarm-script/main/autofarm%20skill%20tree%20script.lua "))() 
 end)
 
-
-Section:NewButton("Dupe Items", "ButtonInfo", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/Dupe-Yba.lua"))()
-end)
-
-
 local Tab = Window:NewTab("ESP")
 
 local Section = Tab:NewSection("ESP")
